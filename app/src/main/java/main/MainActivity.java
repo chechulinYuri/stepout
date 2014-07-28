098a405746a0046af7c9d5ab5cc5c7fe057932a7
Arseny Skurko <ars.skurko@yandex.ru>
2014-07-28 17:30:06 +0700

    need pull

--- app/src/main/java/main/MainActivity.java
+++ app/src/main/java/main/MainActivity.java
@@ -2,68 +2,32 @@ package main;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 
 import com.example.admin.stepout.R;
 
 import model.DataExchange;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     public static final String EVENT_TABLE_NAME = "Event";
     public static final String USER_TABLE_NAME = "User";
+
+    private  LoginFragment loginFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen_layout);
+       /* if (savedInstanceState == null) {
+            loginFragment = new LoginFragment();
+            getSupportFragmentManager().beginTransaction().add(loginFragment, android.R.id.content).commit();
+        }
+        else {
+            loginFragment = (LoginFragment) getSupportFragmentManager().findFragmentById(android.R.id.content)
+        }*/ //NEEDS TO BE FIXED
 
         DataExchange dataExchange = new DataExchange(this);
-
-        // TEST USER SAVE
-        /*User testUser = new User("Yuri", "Chechulin", "+79531233212", "http://cs618726.vk.me/v618726028/d03e/_EawHr8ITXc.jpg", "42323");
-        User usr = dataExchange.saveUserToParseCom(testUser);
-
-        if (usr != null) {
-            Toast toast = Toast.makeText(this, usr.hash, Toast.LENGTH_LONG);
-            toast.show();
-        } else {
-            Toast toast = Toast.makeText(this, "User not saved", Toast.LENGTH_LONG);
-            toast.show();
-        }*/
-
-
-        // TEST USER GET
-        /*User usr = dataExchange.getUserFromParseCom("42323");
-        if (usr != null) {
-            Toast toast = Toast.makeText(this, usr.hash, Toast.LENGTH_LONG);
-            toast.show();
-        } else {
-            Toast toast = Toast.makeText(this, "User not found", Toast.LENGTH_LONG);
-            toast.show();
-        }*/
-
-        // TEST EVENT SAVE
-        /*Event testEvent = new Event("GO ASD", new ParseGeoPoint(40.0, -30.0), Arrays.asList("asd", "qwe"), "dhdhdfhdf", System.currentTimeMillis(), 3);
-        Event evt = dataExchange.saveEventToParseCom(testEvent);
-
-        if (evt != null) {
-            Toast toast = Toast.makeText(this, evt.message + " " + evt.hash, Toast.LENGTH_LONG);
-            toast.show();
-        } else {
-            Toast toast = Toast.makeText(this, "Event not found", Toast.LENGTH_LONG);
-            toast.show();
-        }*/
-
-        //TODO
-
-        // TEST GET EVENTS BY USER HASH
-        /*ArrayList<Event> events = dataExchange.getEventsByUser("asdadqwe123");
-        String res = "";
-        for (int i = 0; i < events.size(); i++) {
-            res += events.get(i).message + " ";
-        }
-        Toast toast = Toast.makeText(this, res, Toast.LENGTH_LONG);
-        toast.show();*/
-
     }
 }
