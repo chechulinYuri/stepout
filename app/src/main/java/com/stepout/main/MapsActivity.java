fdf2a351e0582f6122f11e233d0ddfea68c2d3e7
Yuri Chechulin <chechulin.yuri@gmail.com>
2014-08-05 00:28:48 +0700

    fix google map activity

--- app/src/main/java/com/stepout/main/MapsActivity.java
+++ app/src/main/java/com/stepout/main/MapsActivity.java
@@ -1,10 +1,13 @@
 package com.stepout.main;
 
 import android.app.Activity;
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -18,13 +21,30 @@ public class MapsActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         //setUpMapIfNeeded();
+
+        SupportMapFragment mapFrag = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        GoogleMap map = mapFrag.getMap();
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+
     }
 
-   //@Override
-   //protected void onResume() {
-   //    super.onResume();
-   //    setUpMapIfNeeded();
-   //}
+   @Override
+   protected void onResume() {
+       super.onResume();
+       //setUpMapIfNeeded();
+   }
 
    ///**
    // * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
@@ -41,26 +61,26 @@ public class MapsActivity extends FragmentActivity {
    // * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
    // * method in {@link #onResume()} to guarantee that it will be called.
    // */
-   //private void setUpMapIfNeeded() {
-   //    // Do a null check to confirm that we have not already instantiated the map.
-   //    if (mMap == null) {
-   //        // Try to obtain the map from the SupportMapFragment.
-   //        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-   //                .getMap();
-   //        // Check if we were successful in obtaining the map.
-   //        if (mMap != null) {
-   //            setUpMap();
-   //        }
-   //    }
-   //}
+   private void setUpMapIfNeeded() {
+       // Do a null check to confirm that we have not already instantiated the map.
+       if (mMap == null) {
+           // Try to obtain the map from the SupportMapFragment.
+           mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                   .getMap();
+           // Check if we were successful in obtaining the map.
+           if (mMap != null) {
+               setUpMap();
+           }
+       }
+   }
 
-   ///**
-   // * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-   // * just add a marker near Africa.
-   // * <p>
-   // * This should only be called once and when we are sure that {@link #mMap} is not null.
-   // */
-   //private void setUpMap() {
-   //    mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-   //}
+   /**
+   * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+   * just add a marker near Africa.
+   * <p>
+   * This should only be called once and when we are sure that {@link #mMap} is not null.
+   */
+   private void setUpMap() {
+       mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+   }
 }
