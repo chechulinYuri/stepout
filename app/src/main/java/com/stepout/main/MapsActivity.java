bdb65e20292c0befdf08a7f881dd1e9ad40e1051
Arseny Skurko <ars.skurko@yandex.ru>
2014-08-06 19:11:55 +0700

    map

--- app/src/main/java/com/stepout/main/MapsActivity.java
+++ app/src/main/java/com/stepout/main/MapsActivity.java
@@ -9,6 +9,7 @@ import android.content.IntentSender;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.provider.Settings;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
@@ -20,6 +21,7 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -34,6 +36,8 @@ public class MapsActivity extends FragmentActivity implements
     private SupportMapFragment mapFragment;
     private static GoogleMap map;
     private LocationClient locationClient;
+    private LocationManager service;
+    private boolean allowConnection = true;
     /*
      * Define a request code to send to Google Play services
      * This code is returned in Activity.onActivityResult
@@ -70,27 +74,30 @@ public class MapsActivity extends FragmentActivity implements
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_maps);
 
-        locationClient = new LocationClient(this, this, this);
 
         mapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map));
         map = mapFragment.getMap();
-
         map.setMyLocationEnabled(true);
+        locationClient = new LocationClient(this, this, this);
+        service = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        if (!service.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            TurnOnGpsDialog dialog = new TurnOnGpsDialog();
+            dialog.show(getSupportFragmentManager(), "TurnOnGpsDialog");
+        }
+        else {
+            allowConnection = false;
+            locationClient.connect();
+        }
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        // Connect the client.
-        LocationManager service = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        if (!service.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            TurnOnGpsDialog dialog = new TurnOnGpsDialog();
-            dialog.show(getSupportFragmentManager(), "TurnOnGpsDialog");
-        } else if (isGooglePlayServicesAvailable()){
+        if ((!locationClient.isConnected()) && (service.isProviderEnabled(LocationManager.GPS_PROVIDER)) && (allowConnection)) {
+            allowConnection = false;
             locationClient.connect();
         }
-
     }
 
     /*
@@ -162,10 +169,16 @@ public class MapsActivity extends FragmentActivity implements
     public void onConnected(Bundle dataBundle) {
         // Display the connection status
         Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
-        Location location = null;
+        /*Location location = null;
         while (location == null) {
             location = locationClient.getLastLocation();
-        }
+            //LocationManager asd = (LocationManager)getSystemService(LOCATION_SERVICE);
+            //location = asd.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }*/
+        Location location = locationClient.getLastLocation();
+        locationClient.disconnect();
+        //LocationManager asd = (LocationManager)getSystemService(LOCATION_SERVICE);
+        //location = asd.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
         map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
     }
@@ -235,3 +248,118 @@ public class MapsActivity extends FragmentActivity implements
         }
     }
 }
+/*
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements LocationListener, GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener {
+
+    private GoogleMap mMap;
+    private LocationManager locationManager;
+    private String provider;
+    private double latitude;
+    private double longitude;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            TurnOnGpsDialog dialog = new TurnOnGpsDialog();
+            dialog.show(getSupportFragmentManager(), "TurnOnGpsDialog");
+        }
+
+        LocationClient locationClient = new LocationClient(this, this, this);
+        LocationRequest request = LocationRequest.create();
+        request.setInterval(5 * 1000);
+        request.setPriority(LocationRequest.PRIORITY_LOW_POWER);
+        request.setFastestInterval(5 * 1000);
+        locationClient.connect();
+
+        SupportMapFragment mapFrag = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        GoogleMap map = mapFrag.getMap();
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {}
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {}
+
+    @Override
+    public void onConnected(Bundle bundle) {}
+
+    @Override
+    public void onDisconnected() {}
+
+    @Override
+    public void onProviderEnabled(String provider) {}
+
+    @Override
+    public void onProviderDisabled(String provider) {}
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+    public static class TurnOnGpsDialog extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(R.string.turn_on_gps_dialog)
+                    .setPositiveButton(R.string.settings_text, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivity(intent);
+                        }
+                    })
+                    .setNegativeButton(R.string.cancel_text, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dismiss();
+                        }
+                    });
+            return builder.create();
+        }
+    }
+}
+*/
