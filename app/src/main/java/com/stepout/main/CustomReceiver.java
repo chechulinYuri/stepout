295962d11ff0f39651aa4b782ea756ce03f738a0
Arseny Skurko <ars.skurko@yandex.ru>
2014-08-11 19:22:35 +0700

    test

---
+++ app/src/main/java/com/stepout/main/CustomReceiver.java
@@ -0,0 +1,26 @@
+package com.stepout.main;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Admin on 11.08.2014.
+ */
+public class CustomReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        try {
+            JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
+            String currentEventHash = json.getString(DataExchange.EVENT_HASH_FOR_VIEW_EVENT_ACTIVITY_KEY);
+            Log.d("ASD", currentEventHash);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/com/stepout/main/ViewEventAsGuestActivity.java
+++ app/src/main/java/com/stepout/main/ViewEventAsGuestActivity.java
@@ -17,6 +17,9 @@ import com.parse.SendCallback;
 import com.squareup.otto.Subscribe;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class ViewEventAsGuestActivity extends FragmentActivity {
 
     private Event currentEvent;
@@ -103,6 +106,14 @@ public class ViewEventAsGuestActivity extends FragmentActivity {
             ParsePush push = new ParsePush();
             push.setChannel(currentEvent.getHash());
             push.setMessage(getString(R.string.user_joined_event, currentEvent.getRespondents().size() + 1));
+            //NEW SHIT
+            try {
+                JSONObject data = new JSONObject("{\"action\": \"com.stepout.main.CustomReceiver.SHOW_EVENT\", \"" + DataExchange.EVENT_HASH_FOR_VIEW_EVENT_ACTIVITY_KEY + "\": \"" + currentEvent.getHash() + "\"}");
+                push.setData(data);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            //ENDS HERE
             push.sendInBackground(new SendCallback() {
                 @Override
                 public void done(ParseException e) {
