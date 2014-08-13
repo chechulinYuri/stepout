70cc585f8a16f3820e04af2199ba60d17517a387
Yuri Chechulin <chechulin.yuri@gmailc.com>
2014-08-13 11:00:13 +0700

    asd

--- app/src/main/java/com/stepout/main/CustomReceiver.java
+++ app/src/main/java/com/stepout/main/CustomReceiver.java
@@ -1,7 +1,6 @@
 package com.stepout.main;
 
 import android.app.Activity;
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
